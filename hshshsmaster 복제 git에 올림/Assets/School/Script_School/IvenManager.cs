6be3adec6af33bc65cc1535819f645afc3a455af
lizuAg <wndms058@daum.net>
2020-11-08 01:53:13 +0900

    inventory 최종수정

--- hshshsmaster 복제 git에 올림/Assets/School/Script_School/IvenManager.cs
+++ hshshsmaster 복제 git에 올림/Assets/School/Script_School/IvenManager.cs
@@ -5,13 +5,12 @@ using UnityEngine.UI;
 //Inven여는 스크립트.
 public class IvenManager : MonoBehaviour
 {
-
-    //public static bool baSpray, chuR, ssinssingE, manwon;
-    public Button btnB, btnC, btnS, btnM; 
+    public Button btnB, btnC, btnS, btnM;
+    //public static bool item_kickboard, item_cat, item_bateacher, item_elevator;
     void Awake(){
-        btnB.interactable = Ending_School.baSpray;
-        btnC.interacteble = Ending_School.chuR;
-        btnS.interactable = Eding_School.ssinssingE;
-        btnM.interactable = Ending_School.manwon;
+        btnB.interactable = Ending_School.item_bateacher;
+        btnC.interactable = Ending_School.item_cat;
+        btnS.interactable = Ending_School.item_kickboard;
+        btnM.interactable = Ending_School.item_elevator;
     }
 }
