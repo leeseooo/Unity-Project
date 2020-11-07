add811653e66193c2d9bdee2e78d2222ffb53d63
coriniee <68391767+coriniee@users.noreply.github.com>
2020-11-08 06:22:32 +0900

    시작화면 소리 넣음 - 배경음악, 버튼효과음
    왜 사라졌는지 알 수 없음.. 다시넣음.. 귀에서 모기소리나..

--- hshshsmaster 복제 git에 올림/Assets/Scenes/Room/Script/ButtonSc.cs
+++ hshshsmaster 복제 git에 올림/Assets/Scenes/Room/Script/ButtonSc.cs
@@ -12,44 +12,56 @@ public class ButtonSc : MonoBehaviour
     bool activeCredit = false;
     bool activeHowto = false;
 
+    public AudioSource audio_start;
+
     private void Start()
     {
         AlbumPanel.SetActive(activeAlbum);
         creditPanel.SetActive(activeCredit);
         HowtoPanel.SetActive(activeHowto);
     }
+
+    void Awake(){
+        audio_start = GetComponent<AudioSource>();
+    }
     //게임 시작 버튼
     public void OnStartingButton()
     {
+        audio_start.Play();
         //눈송이 방으로 씬 전환
         SceneManager.LoadScene("room");
     }
     //종료버튼
     public void OnQuitButton()
     {
+        audio_start.Play();
         Application.Quit();
     }
     //앨범 창
     public void OnAlbumButton()
     {
+        audio_start.Play();
         activeAlbum = !activeAlbum;
         AlbumPanel.SetActive(activeAlbum);
     }
     //크레딧 버튼
     public void OnCredit()
     {
+        audio_start.Play();
         activeCredit = !activeCredit;
         creditPanel.SetActive(activeCredit);
     }
     //게임방법 창
     public void OnHowtoButton()
     {
+        audio_start.Play();
         activeHowto = !activeHowto;
         HowtoPanel.SetActive(activeHowto);
     }
     //뒤로가기
     public void ClickedBack()
     {
+        audio_start.Play();
         if (activeAlbum)
             OnAlbumButton();
         else if (activeCredit)
